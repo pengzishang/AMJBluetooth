9a097079e862608305fb598a88b57caf862f2746
pengzishang <357572659@qq.com>
2017-09-15 16:20:47 +0800

    这是啥

--- AMJBluetoothDemo/Pods/Target Support Files/Pods-blueToothTest/Pods-blueToothTest-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_blueToothTest : NSObject
-@end
-@implementation PodsDummy_Pods_blueToothTest
-@end
--- README.md
+++
@@ -1,2 +0,0 @@
-# AMJBluetooth
-爱美家的蓝牙定制
